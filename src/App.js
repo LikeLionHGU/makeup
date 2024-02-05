5b9d971d26fcc64dc08bb84166e32854a57498cf
yeji <yezi0512@handong.ac.kr>
2024-02-05 14:39:27 +0900

    [chore] : 파일 생성

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "./App.css";
+import Main from "./Main";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
