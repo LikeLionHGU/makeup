bfdcff93c905cf8981858909533c2f00424d2f71
Hayoung04 <22300209@handong.ac.kr>
2024-02-06 14:30:47 +0900

    쓸데없는 자료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+// import "./App.css";
+import Main from "./mainpage/Main";
+import Banner from "./banner/Banner";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />}></Route>
+        </Routes>
+        <Routes>
+          <Route path="/" element={<Banner />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
-
 export default App;
