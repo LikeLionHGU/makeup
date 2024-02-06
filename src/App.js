da8e1371deb0fee58fa512ff9f0b899e4f81ded0
yeji <yezi0512@handong.ac.kr>
2024-02-06 22:20:36 +0900

    feat : 검색 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 // import "./App.css";
 import Main from "./Main";
+import Login from "./header/login/Login";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Main />}></Route>
+          <Route path="/login" element={<Login />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/header/Header.js
+++ src/header/Header.js
@@ -1,10 +1,39 @@
 import Login from "./login/Login";
+import BoardList from "./search/BoardList";
+import Search from "./search/Search";
+import { Link } from "react-router-dom";
+import logoimg from "./image/logo.png";
+import chatimg from "./image/chat.png";
+import bellimg from "./image/bell.png";
+import profile from "./image/profile.png";
+import styles from "./Header.module.css";
 
 export default function Header() {
   return (
-    <div>
-      <h2>헤더입니다^^</h2>
-      <Login></Login>
+    <div className={styles.header}>
+      <span className={styles.logo}>
+        <img src={logoimg} />
+      </span>
+      <span id={styles.search}>
+        <Search></Search>
+      </span>
+      <span id={styles.post}>
+        <button>내 메컵 공유</button>
+      </span>
+      <span className={styles.icon}>
+        <span id={styles.chat}>
+          <img src={chatimg} />
+        </span>
+        <span id={styles.notice}>
+          <img src={bellimg} />
+        </span>
+        <span id={styles.profile}>
+          <img src={profile} />
+        </span>
+      </span>
+      {/* <Link to="/login">
+        <button>로그인</button>
+      </Link> */}
     </div>
   );
 }
