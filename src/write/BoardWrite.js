513d0cc4c5ec42df008d2e15cd2c2c3114838512
Hayoung04 <22300209@handong.ac.kr>
2024-02-17 02:32:41 +0900

    style : 게시글 스타일 싹 바꿈

--- src/write/BoardWrite.js
+++ src/write/BoardWrite.js
@@ -56,67 +56,58 @@ const BoardWrite = () => {
       </Routes>
 
       <div className={styles.rect}>
-        {image ? null : (
-          <span className={styles.button}>
-            <input
-              type="file"
-              style={{ display: "none" }}
-              ref={imageInput}
-              onChange={handleImageChange}
-            />
-            <button onClick={onCickImageUpload}>
-              <img alt="photo01" src={photoimg} />
-            </button>
-          </span>
-        )}
-        {image && (
-          <span className={styles.photo}>
-            <img
-              src={image}
-              alt="Uploaded"
-              style={{
-                maxWidth: "460px",
-                maxHeight: "580px",
-                borderRadius: "40px",
-                marginTop: "25px",
-                marginBottom: "25px",
-                marginLeft: "25px",
-              }}
-            />
-          </span>
-        )}
-
-        <span className={styles.title}>
-          <textarea
-            type="text"
-            name="title"
-            value={title}
-            cols="20"
-            rows="10"
-            onChange={onChange}
-            placeholder="제목 입력하기"
-          />
-        </span>
-        <span className={styles.contents}>
-          <textarea
-            name="contents"
-            cols="50"
-            rows="20"
-            value={contents}
-            onChange={onChange}
-            placeholder="사용한 화장품 리스트를 알려주세요."
-          ></textarea>
-        </span>
-
-        <div className={styles.buttonContainer}>
-          <div className={styles.line}></div>
-          <span className={styles.save}>
-            <button onClick={saveBoard}>작성 완료</button>
-          </span>
-          <span className={styles.date}>
-            <button onClick={saveBoard}>날짜 설정</button>
-          </span>
-          {/* 날짜 설정 페이지로 이동하기 */}
+        <div className={styles.left}>
+          {image ? (
+            <div className={styles.photo}>
+              <img src={image} alt="Uploaded" />
+            </div>
+          ) : (
+            <div className={styles.button}>
+              <input
+                type="file"
+                style={{ display: "none" }}
+                ref={imageInput}
+                onChange={handleImageChange}
+              />
+              <button onClick={onCickImageUpload}>
+                <img alt="photo01" src={photoimg} />
+              </button>
+            </div>
+          )}
+        </div>
+        <div className={styles.right}>
+          <div className={styles.titlecontent}>
+            <div className={styles.title}>
+              <textarea
+                type="text"
+                name="title"
+                value={title}
+                rows="2"
+                onChange={onChange}
+                placeholder="제목 입력하기"
+              />
+            </div>
+            <div className={styles.contents}>
+              <textarea
+                name="contents"
+                cols="50"
+                rows="20"
+                value={contents}
+                onChange={onChange}
+                placeholder="사용한 화장품 리스트를 알려주세요."
+              ></textarea>
+            </div>
+          </div>
+          <div className={styles.buttonContainer}>
+            <div className={styles.line}></div>
+            <div className={styles.save}>
+              <button onClick={saveBoard}>작성 완료</button>
+            </div>
+            <div className={styles.date}>
+              <button onClick={saveBoard}>날짜 설정</button>
+            </div>
+            {/* 날짜 설정 페이지로 이동하기 */}
+          </div>
         </div>
       </div>
     </div>
